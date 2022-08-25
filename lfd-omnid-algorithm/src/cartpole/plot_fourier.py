d0fa6fc53d782997f45ca4313d2b2d313a99a820
Ishaan Narain <ishaannarain2022@u.northwestern.edu>
2022-08-25 17:03:54 -0500

    Updating fourier visualizations

--- lfd-omnid-algorithm/src/cartpole/plot_fourier.py
+++ lfd-omnid-algorithm/src/cartpole/plot_fourier.py
@@ -17,20 +17,25 @@ def get_phix(x, K, phik_dict, ergodic_helper):
 
 
 def fourier_plot_2dim():
-    num_trajec = 1
+    num_trajec = 13
+    demonstration_list = [0, 6, 7, 10, 11, 12]
 
-    K = 10
+    K = 8
     L = [[-np.pi, np.pi], [-11, 11]]
     dt = 0.01
-    E = [1, -1, -1, -1, -1, -1, -1, -1]
+    E = [-1, -1, -1, -1, -1, -1, 1, 1, 1, -1, -1, -1, -1]
+    new_E = []
     D = []
     for i in range(num_trajec):
+        if i not in demonstration_list:
+            continue
+        new_E.append(E[i])
         demonstration = np.genfromtxt(f'src/cartpole_gazebo/dynamics/test{i}.csv', delimiter=',')
         D.append(demonstration)
     D = [np.array(d)[:, :2] for d in D]
-    E = E[:num_trajec]
+    # E = E[:num_trajec]
 
-    ergodic_helper = ErgodicHelper(D, E, K, L, dt)
+    ergodic_helper = ErgodicHelper(D, new_E, K, L, dt)
     _, phik_dict, _ = ergodic_helper.calc_fourier_metrics()
 
     x = np.linspace(-np.pi, np.pi, 50)
@@ -38,7 +43,7 @@ def fourier_plot_2dim():
     z = np.array([get_phix([i, j], K, phik_dict, ergodic_helper) for j in y for i in x])
     Z = z.reshape(50, 50)
 
-    plt.imshow(Z, interpolation='bilinear', vmin=-0.1, vmax=0.5)
+    plt.imshow(Z, interpolation='none', vmin=-0.1, vmax=0.5, extent=[-3.14159, 3.14159, -15, 15], aspect='auto')
     plt.show()
 
 
